7c92ace04d68153c65b3cc50576cd8127894ec8c
Bart6arian <zemstabartosz@gmail.com>
2022-04-07 14:00:21 +0200

    calc for task 1.3 done

--- module-1/src/main/java/com/practice/multi/multiply/App.java
+++ module-1/src/main/java/com/practice/multi/multiply/App.java
@@ -1,13 +1,35 @@
 package com.practice.multi.multiply;
 
+import com.practice.multi.executor.Task;
+
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class App {
 
     public static void main(String[] args) {
-        int[] theArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        /*int[] theArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         SimpleCalc calc = new SimpleCalc(theArray);
         calc.run();
         int sum = calc.getSum();
-        System.out.println("Sum of first calc = " + sum);
+        System.out.println("Sum of first calc = " + sum);*/
+        // task 1.2
+
+
+        int cores = Runtime.getRuntime().availableProcessors();
+        ExecutorService executorService = Executors.newFixedThreadPool(cores);
+        int[] theArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] theArray2 = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+        SimpleCalc2 calc = new SimpleCalc2(theArray, theArray2);
+
+        for (int n = 0; n < cores; n++) {
+            calc.run();
+        }
+        System.out.println(Arrays.toString(calc.getScores()));
+
+        executorService.shutdown();
     }
 }
---
+++ module-1/src/main/java/com/practice/multi/multiply/SimpleCalc2.java
@@ -0,0 +1,38 @@
+package com.practice.multi.multiply;
+
+import java.util.Arrays;
+
+public class SimpleCalc2 implements Runnable {
+
+    private int[] elems;
+    private int[] elems2;
+    private long[] scores;
+
+    public SimpleCalc2(int[] elems, int[] elems2) {
+        this.elems = elems;
+        this.elems2 = elems2;
+    }
+
+    private void executeMath() {
+        multiplyByValueOfOtherArray(elems, elems2);
+    }
+
+    private void multiplyByValueOfOtherArray(int[] elems, int[] elems2) {
+
+        long[] tempResult = new long[elems.length];
+        for (int i = 0; i < elems.length; i++) {
+            tempResult[i] = (long) elems[i] * elems2[i];
+        }
+        scores = tempResult;
+    }
+
+    public long[] getScores() {
+        return scores;
+    }
+
+    @Override
+    public void run() {
+        executeMath();
+        getScores();
+    }
+}
